b7bdeaa8ec1ab7695e278e7c531bded2acf8e1cf
Aycan <aycan.acn2020@gmail.com>
2024-06-03 21:53:54 +0200

    add basic layout for page

--- webpack.common.js
+++ webpack.common.js
@@ -8,11 +8,20 @@ module.exports = {
   plugins: [
     new HtmlWebpackPlugin({
       title: "Production",
+      template: "./src/index.html",
     }),
   ],
   output: {
     filename: "[name].bundle.js",
     path: path.resolve(__dirname, "dist"),
     clean: true,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ["style-loader", "css-loader"],
+      },
+    ],
   },
 };
