6be0b2d6e83e6e8f1dbf4b7f3179cee9a1aec05a
Aycan <aycan.acn2020@gmail.com>
2024-06-04 12:26:24 +0200

    add function that removes previous content before updating

--- src/index.js
+++ src/index.js
@@ -2,6 +2,11 @@ import style from "./style.css";
 import createHomeContent from "./homePage";
 import createMenuContent from "./menuPage";
 
+function removePreviousContent() {
+  const content = document.querySelector("#content");
+  content.innerHTML = "";
+}
+
 createHomeContent();
 
 const homeBtn = document.querySelector("#home");
@@ -9,14 +14,17 @@ const menuBtn = document.querySelector("#menu");
 const aboutBtn = document.querySelector("#about");
 
 homeBtn.addEventListener("click", () => {
+  removePreviousContent();
   createHomeContent();
 });
 
 menuBtn.addEventListener("click", () => {
+  removePreviousContent();
   createMenuContent();
 });
 
 aboutBtn.addEventListener("click", () => {
   // not done
+  //removePreviousContent();
   console.log("not done");
 });
