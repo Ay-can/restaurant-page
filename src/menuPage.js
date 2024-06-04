34ef438d2514651b7b4090849827d33279d07f03
Aycan <aycan.acn2020@gmail.com>
2024-06-04 16:14:57 +0200

    add menu item and basic styling

--- src/menuPage.js
+++ src/menuPage.js
@@ -1,13 +1,14 @@
+import pepperoniPizza from "./pepperoni-pizza.png";
+
 export default function createMenuContent() {
   const content = document.querySelector("#content");
   const h1 = document.createElement("h1");
-  const p = document.createElement("p");
   const menuItemsContainer = document.createElement("div");
+  const img = new Image(300, 300);
 
-  h1.innerText = "Pepperoni Pizza";
-  p.innerText = `Lorem ipsum, dolor sit amet consectetur adipisicing elit. Iusto quasiassumenda voluptas autem necessitatibus harum eaque sed illum illo doloremque!`;
+  h1.innerText = "Our Delicious Menu";
   content.appendChild(h1);
-  content.appendChild(p);
+  menuItemsContainer.classList.add("menu-container");
 
   // create a menu item
   const firstMenuItem = document.createElement("div");
@@ -18,9 +19,13 @@ export default function createMenuContent() {
 
   firstMenuItemTitle.innerText = "Pepperoni Pizza";
   firstMenuItemDescription.innerText = `Lorem ipsum, dolor sit amet consectetur adipisicing elit. Iusto quasi assumenda voluptas autem necessitatibus harum eaque sed illum illo doloremque!`;
+  img.src = pepperoniPizza;
 
   firstMenuItem.append(firstMenuItemTitle);
   firstMenuItem.append(firstMenuItemDescription);
+  firstMenuItem.append(img);
 
   menuItemsContainer.append(firstMenuItem);
+
+  content.appendChild(menuItemsContainer);
 }
